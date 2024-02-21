87a1458d773461372ffa0e2e2243053b557aa574
Teddy Robinson <contactteddyrobinson@gmail.com>
2024-02-21 08:32:19 +0100

    prompt generator mechanism made

--- src/Pages/PromptGen.tsx
+++ src/Pages/PromptGen.tsx
@@ -7,8 +7,18 @@ const PromptGen = () => {
 const [promptRes, setPromptRes] = useState('')
 const [b1, setB1] = useState('unclicked')
 
-const [titleRes, setTitleRes] = useState('')
+const [titleRes, setTitleRes] = useState<any>('')
 const [b2, setB2] = useState('unclicked')
+
+const [structureRes, setStructureRes] = useState<any>('')
+const [b3, setB3] = useState('unclicked')
+
+const [instRes, setInstRes] = useState<any>('')
+const [b4, setB4] = useState('unclicked')
+
+const [vocRes, setVocRes] = useState<any>('')
+const [b5, setB5] = useState('unclicked')
+
 
 //Things to keep in mind:
 //Choose wisely what is and isn't open for interpretation. 
@@ -22,17 +32,16 @@ const prompt = [
 
 // The title, will be three random words, so just add whatever words you want for vibe. Pay close attention to them during writing.
 const title = [
-  'Mist',
-  'Cloud',
-  'Moon',
-  'Psycho',
-  'Killer',
-  'River',
-  'Hermes',
+  'Mist ',
+  'Cloud ',
+  'Moon ',
+  'Psycho ',
+  'Killer ',
+  'River ',
+  'Hermes ',
 ]
 
 // The structure decides the path the song will take during its course, or the dynamics of the environment
-//Can be multiple?
 const structure = [
   'Follows a clear setup, conflict, resolution / lack of resolution',
   'Is told as memories are, without regard for chronology, and with all the elements out of proportion',
@@ -40,6 +49,9 @@ const structure = [
   'Loud during the verses, but quiet during the chorus',
   'The closer you get to the character the more reverb there is. Objects and events can be characters, too.',
   'Every time the mood changes, so does the room.',
+  'Simple pop structure: verses, hooks and bridge',
+  'Simple pop structure: verses, hooks and bridge',
+
 ]
 
 // Artists, producers and songs to take inspiration from during the production
@@ -52,16 +64,14 @@ const instArtist = [
   'Mac Demarco - Chamber Of Reflection',
   'Beach House',
   'Ian Kirkpatrick',
-  'Andy Shauf'
 ]
 
 // Modifiers for the preceding, comes after the word "but"
-// 1 in 3 chance of appearing
 const instMod = [
-  'with Hyperpop influences',
-  'made with the bare minimum',
-  'with a crazy shuffle',
-
+  ' with Hyperpop influences',
+  ' made with the bare minimum',
+  ' with a crazy shuffle',
+  ' made on a cheap cassette tape'
 ]
 
 // Vocal artists and songs to take inpiration from during writing
@@ -73,13 +83,13 @@ const vocArtist = [
   'Nico',
   'James Blake',
   'Frank Ocean',
+  'Andy Shauf'
 ]
 
 // Modifiers for the preceding, , comes after the word "but"
-// 1 in 3 chance of appearing
 const vocMod = [
-  'with a vintage mic setup',
-  'recorded like Blonde',
+  ' with a vintage mic setup',
+  ' with Blonde vocal chops',
 ]
 
 
@@ -89,25 +99,55 @@ const promptClick = () => {
   setB1('clicked')
 }
 
-// function randomNoRepeats(array: any) {
-//   var copy = array.slice(0);
-//   return function() {
-//     if (copy.length < 1) { copy = array.slice(0); }
-//     var index = Math.floor(Math.random() * copy.length);
-//     var item = copy[index];
-//     copy.splice(index, 1);
-//     return item;
-//   };
-// }
-
 const titleClick = () => {
-  const rng = Math.floor(Math.random() * prompt.length)
+  const rng = 2 + Math.floor(Math.random() * 2)
+  
+  let endTitle = []
+  let num: number
 
-  setTitleRes('')
+  for (let i = 0; i < rng; i++) {
+    num = Math.floor(Math.random() * title.length) 
+    endTitle.push(title[num])
+    title.splice(num, 1)
+  }
+
+  setTitleRes(endTitle)
   setB2('clicked')
 }
 
+const structureClick = () => {
+  const rng = Math.floor(Math.random() * structure.length)
+  setStructureRes(structure[rng])
+  setB3('clicked')
+}
 
+const instClick = () => {
+  const rng = Math.floor(Math.random() * instArtist.length)
+  const rng2 = Math.floor(Math.random() * instMod.length)
+  const rng3 = Math.random()
+  if (rng3 < 0.2) {
+    const result = instArtist[rng] + instMod[rng2]
+    setInstRes(result)
+  } else {
+    setInstRes(instArtist[rng])
+  }
+  
+  setB4('clicked')
+}
+
+const vocClick = () => {
+  const rng = Math.floor(Math.random() * vocArtist.length)
+  const rng2 = Math.floor(Math.random() * vocMod.length)
+  const rng3 = Math.random()
+  if (rng3 < 0.2) {
+    const result = vocArtist[rng] + vocMod[rng2]
+    setVocRes(result)
+  } else {
+    setVocRes(vocArtist[rng])
+  }
+  
+  setB5('clicked')
+}
 
 
 
@@ -124,9 +164,24 @@ const titleClick = () => {
       <div>{promptRes}</div>
       <br/><br/>
 
-      <h5>Prompt</h5>
-      <Button variant="primary" className={b1} onClick={() => titleClick()}>Generate</Button>
+      <h5>Title</h5>
+      <Button variant="primary" className={b2} onClick={() => titleClick()}>Generate</Button>
       <div>{titleRes}</div>
+      <br/><br/>
+
+      <h5>Structure</h5>
+      <Button variant="primary" className={b3} onClick={() => structureClick()}>Generate</Button>
+      <div>{structureRes}</div>
+      <br/><br/>
+
+      <h5>Instrumental inspo</h5>
+      <Button variant="primary" className={b4} onClick={() => instClick()}>Generate</Button>
+      <div>{instRes}</div>
+      <br/><br/>
+
+      <h5>Vocal inspo</h5>
+      <Button variant="primary" className={b5} onClick={() => vocClick()}>Generate</Button>
+      <div>{vocRes}</div>
       <br/><br/>
 
       <div>
